e68beb163b3f1b36fd7edc611127aeee0b2f5e70
lloveland2 <lloveland2@ivytech.edu>
2023-07-13 12:37:26 -0400

    Finished the django tutorial on HTML.
    - Modified blog/views.py
    - Added new file: blog/templates/blog/post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+"""Module Docstring Goes Here"""
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    """Function Docstring Goes Here"""
+    return render(request, 'blog/post_list.html', {})
